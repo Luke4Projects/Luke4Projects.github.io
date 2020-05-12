5af683c135e985d6b21a1c65f2793007fb2573a2
Luke4Projects <65132042+Luke4Projects@users.noreply.github.com>
2020-05-11 20:48:35 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 function moveToSkyDodge() {
     location.href = 'https://luke4projects.github.io/SkyDodge'
 }
+function goToPong() {
+    location.href = 'https://luke4projects.github.io/Pong';
+}
+function howToPlaySkyDodge() {
+    document.write("<h1>test</h1>");
+}
